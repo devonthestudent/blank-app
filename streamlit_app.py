d4aa85bb4e45e1891a5a9c68e32dde8b9d0a5d1a
devonthestudent <64247274+devonthestudent@users.noreply.github.com>
2025-03-11 16:09:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,8 +86,8 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder = st.empty()
             full_response = ''
             for item in response:
-                st.write(item)
-                full_response += item.choices[0].delta.content
+                part = item.choices[0].delta.content or ""
+                full_response += part
                 placeholder.markdown(full_response)
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
