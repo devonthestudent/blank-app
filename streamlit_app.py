e5be326d5e90be1c637b4cf29ef0c163d392998b
devonthestudent <64247274+devonthestudent@users.noreply.github.com>
2025-03-11 14:50:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Chatgpt Chatbot')
+    st.title('🦙💬 John Smith Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
